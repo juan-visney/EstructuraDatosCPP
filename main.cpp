102a4b25d6ea56b640349bf769e160a1f25491d4
Juan Visney <jv575juan@gmail.com>
2022-06-28 21:17:21 -0400

    ABB insercion de datos

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
-#include <iostream>
+#include "Arbol.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world" << endl;
+    ABB arbol;
+    arbol.insertar(10);
+    arbol.insertar(5);
+    arbol.insertar(1);
+    arbol.insertar(8);
+    arbol.insertar(14);
+    arbol.insertar(11);
+    arbol.insertar(19);
+    arbol.preOrden();
+    arbol.enOrden();
+    arbol.postOrden();
     return 0;
 }
